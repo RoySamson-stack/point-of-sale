a1a17b526e189009dbe9b301391a2fea87f8f273
RoySamson-stack <rsam9009@gmail.com>
2022-05-18 12:19:59 +0300

    making the introductio to the shpping system to user

---
+++ salesystem.py
@@ -0,0 +1,24 @@
+def pointOfSale():
+  
+
+  #Welcoming the customer and printing out the option of products available
+  print("Welcome to Onn the way supermarket")
+  print("1. Bread 50 \n2Eggs 15 each \n3Milk 55 \nMaizeFlour 120");
+
+
+  # item = input("Enter the option of the goods (1 for bread)")
+  # amount = input("Enter the quantity needed")
+
+
+  # if item == 1:
+  #   price = 50
+  #   total = amount * price
+  #   print("Bread and the quantity is " + str(amount))
+  #   print("Your total is" + str(total))
+
+
+def main():
+  pointOfSale()
+ 
+
+main()
