be212fa8546c6154d91abb64cd5b35166f264583
RoySamson-stack <rsam9009@gmail.com>
2022-05-19 14:23:33 +0300

    making the object for the order and append it

--- salesystem.py
+++ salesystem.py
@@ -1,5 +1,7 @@
 class pointOfSale:
-  def __init__(self, total=0):
+  def __init__(self, total, name,):
+    self.name = name
+    self.orders = []
     self.total = 0
     self.customer()
     self.shop()
@@ -7,15 +9,17 @@ class pointOfSale:
     recieve = int(input("Input Money Recieve:\n"))
     print("Total:", self.total)
     print("Change:",recieve - self.total)
-    print("*****Thank You Come Again!!!*****")
+    print("*Thank You Come Again!!!*")
     quit()
     
- 
+  def get_name(self):
+    return self.name
       
   def customer(self):
     print("Welcome to Onn the way supermarket")
     print("1. Bread 50 \n2.Eggs 15 \n3.Milk 55 \n4.Maize flour 120 \n5.Cash Out \n6.Exit")
     
+    name = input("Input your name:")
     item = input("Enter the option")
     quantity = input("Enter the quantity")
     
@@ -38,6 +42,10 @@ class pointOfSale:
     self.total = self.total + total
     print("All done")
    
+  def order(self, order):
+    if self.order > 0:
+      self.orders.append(order)
+      print("Order:", order)
     
 def main():
   a = pointOfSale()
