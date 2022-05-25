7e6eae98cfe490e8056033c14681f06045f5cc3d
RoySamson-stack <rsam9009@gmail.com>
2022-05-25 22:50:50 +0300

    making the delete only one if names are same

--- customer.py
+++ customer.py
@@ -88,29 +88,33 @@ def update_customer():
 
 
 def delete_customer():
-  name=input("Enter the name to search ").strip()
+  name = input("Enter the name you want to search")
+  
+  name_a = []
   newdata = ""
-  f = open("customer.txt", "r+")
+  f = open("customer.txt","r")
   file = f.readlines()
-  # for line in f:
-  #     if re.search(name, line):
-  #         names=line.split()
+  for line in open("customer.txt", 'r'):
+      if re.search(name, line):
+          ages=line.split()
+          name_a.append(ages)
+  print(name_a)
+  id = input("Enter the id to delete")       
   for line in file:
-    if names+"\n" in line:
+    if id+"\n" in line:
       #strip the line that has a match with the name theat has been entered
       words=line.strip(line)
       #removing the line completely from the file
-      file.remove(name+"\n")
+      file.remove(id+"\n")
     if name in line:
       file.remove(line)
+  print("Successfully deleted")    
   print(file)
-  for name in file:
-    newdata += name
+  for id in file:
+    newdata += id
 
   f = open("customer.txt", "w")
-  f.write(newdata)   
-
- 
+  f.write(newdata)
   
 #upddating the customer in the list
 
--- test.py
+++ test.py
@@ -1,12 +1,30 @@
 import re
 
 def searchage():
-  name = input("Ente rthe name you wan tot search")
-  age = []
-  f = open("customer.txt", "r+")
+  name = input("Enter the name you want to search")
+  
+  name_a = []
+  newdata = ""
+  f = open("customer.txt","r")
   file = f.readlines()
-  for line in f:
+  for line in open("customer.txt", 'r'):
       if re.search(name, line):
-          name=line.split()
+          ages=line.split()
+          name_a.append(ages)
+  print(name_a)
+  id = input("Enter the id to delete")       
+  for line in file:
+    if id+"\n" in line:
+      #strip the line that has a match with the name theat has been entered
+      words=line.strip(line)
+      #removing the line completely from the file
+      file.remove(id+"\n")
+    if name in line:
+      file.remove(line)
+  print(file)
+  for id in file:
+    newdata += id
 
+  f = open("customer.txt", "w")
+  f.write(newdata)  
 searchage()   
