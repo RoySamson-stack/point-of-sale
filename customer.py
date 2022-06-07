f529f918c5efabac447412bd4f23ac2bd1bc3189
RoySamson-stack <rsam9009@gmail.com>
2022-06-07 14:39:49 +0300

    updating the update function for the customers

--- customer.py
+++ customer.py
@@ -91,71 +91,65 @@ def update_customer():
   print("1. Name update \n2. Address update \n3. Phonenumber update \n4. Update all \n5. Exit")
   option=int(input("Enter the option you want pt update"))
   f = open("customer.txt", "r").readlines()
-  file = open("customer.txt", "w")
+  # file = open("customer.txt", "w")
   if option == 1:
-        search=input("Enter the name to search (copy the id)")
-        for line in open("customer.txt", "r"):
-          if re.search(search, line):
-            name_search=line.split()
-            print(name_search)
-        id = input("Paste the id")
-        for line in open("customer.txt", "r+"):
-          if re.search(id, line):
-            old_name = search
-            f = open("customer.txt", "r+")
-            words=line.split()
-            print(words)
-            word = str(words)
-            new_name=input("Enter the new name")
-            new_data  = name_search.remove(name_search[2])
-            name_search[2] = new_name
-            new_data=word.replace(old_name, new_name)
-            print(str(new_data))
-            # file.remove(name_search)
-            file.writelines(new_data)
-            #search for the line name and remove 
-            file = open("customer.txt", "r")
-            lines = file.readlines()
-            for line in file:
-              if line.match(name_search):
-                line.strip()
+      file = open("customer.txt", "r")
+      fr = file.readlines()
+      id = input("Enter the customer id: ")
+      for line in open("customer.txt", "r"):
+        if re.search(id, line):
+          words=line.split()
+          print(words)
+          new_name = input("Enter the new name: ")
+          old_name = words[2]
+          string = " " 
+          new_file = string.join(fr)
+          new_data = new_file.replace(old_name, new_name)
+          # print(new_file)
+          # print(new_data)
+          
+          with open("customer.txt", "w") as f:
+            f.write(new_data)
+        
               
   elif option == 2:
-        f = open("customer.txt", "r+")
-        name = input("Enter the name (copy the id)")
-        for line in open("customer.txt", "r+"):
-            if re.search(name, line):
-              words=line.split()
-              print(words)
-              #asking user for id to get the correct user to avoid updating wrong user
-              id=input("Copy and paste the id")
-              for line in open("customer.txt", "r+"):
-                if re.search(id, line):
-                  word_s=line.split()
-                  print(word_s)
-              address = input("Enter/Copy and paste the address")
-              word = str(words)
-              new_address=input("Enter the new address")
-              #repace the old address with the new address
-              new_data=word.replace(address, new_address)
-              print(new_data)
-              f.writelines(new_data)
+      file = open("customer.txt", "r")
+      fr = file.readlines()
+      id = input("Enter the customer id: ")
+      for line in open("customer.txt", "r"):
+        if re.search(id, line):
+          words=line.split()
+          print(words)
+          new_address = input("Enter the new address: ")
+          old_address = words[4]
+          string = " " 
+          new_file = string.join(fr)
+          new_data = new_file.replace(old_address, new_address)
+          # print(new_file)
+          # print(new_data)
+          
+          with open("customer.txt", "w") as f:
+            f.write(new_data)
 
   elif option == 3:
-        new_info = ""
-        name = input("Enter the name")
-        for line in open("customer.txt", "r+"):
-            if re.search(name, line):
-              words=line.split()
-              print(words)
-              phonenumber = input("Enter/Copy and paste the phonenumber")
-              word = str(words)
-              new_phonenumber=input("Enter the new address")
-              new_data=word.replace(phonenumber, new_phonenumber)
-              # print(new_data)
-              with open("customer.txt", "a") as f:
-                new_info += str(new_data)
-                f.writelines(new_info)
+      file = open("customer.txt", "r")
+      fr = file.readlines()
+      id = input("Enter the customer id: ")
+      for line in open("customer.txt", "r"):
+        if re.search(id, line):
+          words=line.split()
+          print(words)
+          new_number = input("Enter the new number: ")
+          old_number = words[6]
+          string = " " 
+          new_file = string.join(fr)
+          new_data = new_file.replace(old_number, new_number)
+          # print(new_file)
+          # print(new_data)
+          
+          with open("customer.txt", "w") as f:
+            f.write(new_data)
+            
   elif option == 4:
           name=input("Enter the name to search ").strip()
           for line in open("customer.txt", "r+"):
